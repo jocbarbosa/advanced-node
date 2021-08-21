dd785f93fa43c00fd9f41e80113d94be553637ce
Joao C Barbosa <joaao@outlook.com>
2021-08-21 15:21:34 -0300

    test: ensure service calls facebookApi with correct params

--- src/domain/features/facebook-authentication.ts
+++ src/domain/features/facebook-authentication.ts
@@ -1,7 +1,7 @@
 import { AccessToken } from '@/domain/models';
 import { AuthenticationError } from '@/domain/errors';
 
-namespace FacebookAuthentication {
+export namespace FacebookAuthentication {
     export type Params = {
         token: string;
     }
