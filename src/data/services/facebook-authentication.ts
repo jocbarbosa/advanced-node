6c1c61c06c9ab7a10ed52d28da5f4daef5cd5892
Joao C Barbosa <joaao@outlook.com>
2022-01-30 20:17:23 -0300

    refactor: move update to general method

--- src/data/services/facebook-authentication.ts
+++ src/data/services/facebook-authentication.ts
@@ -1,32 +1,22 @@
 import { LoadFacebookUserApi } from '@/data/contracts/apis';
 import { AuthenticationError } from '@/domain/errors/';
 import { FacebookAuthentication } from '@/domain/features';
-import { LoadUserAccountRepository, CreateFacebookAccountRepository, UpdateFacebookAccountRepository } from '../contracts/repos';
+import { FacebookAccount } from '@/domain/models/facebook-account';
+import { LoadUserAccountRepository, SaveFacebookAccountRepository } from '../contracts/repos';
 
 export class FacebookAuthenticationService {
   constructor(
     private readonly facebookApi: LoadFacebookUserApi,
     private readonly userAccountRepo: LoadUserAccountRepository
-    & CreateFacebookAccountRepository
-    & UpdateFacebookAccountRepository,
+    & SaveFacebookAccountRepository,
   ) {}
 
   async perform(params: FacebookAuthentication.Params): Promise<AuthenticationError> {
     const facebookData = await this.facebookApi.loadUser(params);
     if (facebookData !== undefined) {
-      const accountData = await this.userAccountRepo.load({
-        email: facebookData.email,
-      });
-
-      if (accountData !== undefined) {
-        await this.userAccountRepo.updateWithFacebook({
-          id: accountData.id,
-          name: accountData.name ?? facebookData.name,
-          facebookId: facebookData.facebookId,
-        });
-      } else {
-        await this.userAccountRepo.createFromFacebook(facebookData);
-      }
+      const accountData = await this.userAccountRepo.load({ email: facebookData.email });
+      const facebookAccount = new FacebookAccount(facebookData, accountData);
+      await this.userAccountRepo.saveWithFacebook(facebookAccount);
     }
     return new AuthenticationError();
   }
